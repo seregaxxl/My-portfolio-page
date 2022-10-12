18ec9d0fc5fe38dcf6c43582604c92378acbddb5
seregaxxl <seregaxxl@gmail.com>
2022-10-12 12:35:18 +0300

    in-between adaptation first screen done second in work

--- src/js/script.js
+++ src/js/script.js
@@ -16,4 +16,21 @@ const counters = document.querySelectorAll('.scale_percent'),
 
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
-}) 
+});
+
+// let promo__subtitle = document.querySelector('.promo__subtitle');
+// let subtitle_height = promo__subtitle.offsetHeight;
+
+// let promo__title = document.querySelector('.promo__title');
+// let title_height = '50vh';
+
+// // promo__title.offsetHeight;
+
+// const r = document.querySelector(':root');
+// r.style.setProperty('--t_heigth', title_heigh);
+
+// function myFunction_set() {
+//     r.style.setProperty('--t_heigth', '50vh');
+//     r.style.setProperty('--st_heigth', '50vh');
+//   };
+
